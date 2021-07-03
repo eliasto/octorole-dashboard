07d1b3c09bf9012949f7b6b64150931fba0872de
TOURNEUX Elias <55650958+eliasto@users.noreply.github.com>
2021-07-03 22:05:03 +0200

    Update Front.js

--- src/pages/Front.js
+++ src/pages/Front.js
@@ -77,7 +77,7 @@ function Front() {
   const navigation = [
     { name: 'Documentation', href: 'https://doc.octorole.xyz' },
     { name: 'Serveur Discord officiel', href: 'https://octorole.xyz/discord' },
-    { name: 'Status', href: 'https://status.octorole.xyz' },
+    { name: 'Status', href: 'http://status.octorole.xyz' },
   ]
 
   const footer = [
